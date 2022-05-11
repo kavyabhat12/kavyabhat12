e50fea4c6b0889c8d8f8e64d8cad265d8405dc00
kavya bhat <98947329+kavyabhat12@users.noreply.github.com>
2022-05-11 16:33:23 +0530

    Add files via upload

--- kruskal.c
+++ kruskal.c
@@ -1,88 +1,72 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int cost[15][15];
-int n,mincost,v1=0,v2=0;
-
-void findmin()
- {
-    int ewt=99,i,j;
-    for(i=0;i<n;i++)
-       {
-        for(j=0;j<n;j++)
-          {
-            if(cost[i][j]>0 && cost[i][j]>ewt)
-              {
-               ewt=cost[i][j];
-               v1=i;
-               v2=j;
-              }
-           }
-         }
- }
+int cost[10][10],n,mincost,v1=0,v2=0;
 
 void doUnion(int root[],int v1,int v2)
 {
-int temp,i;
-temp=root[v2];
-for(i=0;i<n;i++)
+int temp=root[v2];
+for(int i=0;i<n;i++)
 {
 if(root[i]==temp)
-{
 root[i]=root[v1];
 }
 }
+void findMin()
+{
+	int edgewt=99,i,j;
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+		if(cost[i][j]>0 && cost[i][j]<edgewt)
+		{
+			edgewt=cost[i][j];
+			v1=i;
+			v2=j;
+		}
+		}
+	}
 }
 
 void kruskal()
- {
-   int i,ewt;
-   int root[n];
-   for(i=0;i<n;i++)
-   root[i]=i;
-   printf("\n Edged of minimum-cost spanning tree");
-   i=0;
-   while(i!=n-1)
-        {
-         findmin();
-         ewt=cost[v1][v2];
-         cost[v1][v2]=cost[v2][v1]=0;
-         if(root[v1]!=root[v2])
-           {
-             printf("\n(%d,%d)",v1,v2);
-             doUnion(root,v1,v2);
-             mincost+=ewt;
-             i++;
-           }     
-        }
-printf("\n cost of min-cost spanning tree = %d",mincost);
- }
-
+{
+	int i,edgewt;
+	int root[n];
+	for(i=0;i<=n;i++)
+		root[i]=i;
+	printf("\nEdges of mini-cost speanning tree are:");
+	i=0;
+	while(i!=n-1)
+	{
+		findMin();
+		edgewt=cost[v1][v2];
+		cost[v1][v2]=cost[v1][v2]=0;
+		if(root[v1]!=root[v2])
+		{
+			printf("\n(%d%d)",v1,v2);
+			doUnion(root,v1,v2);
+			mincost+=edgewt;
+			i++;
+		}
+	}
+	printf("\nCost of mincost spanning tree: %d",mincost);
+	
+}
 void main()
- {
-  int i,j;
-  printf("Enter thenumber of vertices:");
-  scanf("%d",&n);
-  printf("Enter the undirected graph as adjacency matrix:\n");
-
-  for(i=0;i<n;i++)
-     {
-      for(j=0;j<n;j++)
-         {
-           scanf("%d\t",&cost[i][j]);
-          }
-}
- for(i=0;i<n;i++)
-    {
-     for(j=0;j<n;j++)
-        {
-         printf("%d\t",cost[i][j]);
-        }
-       printf("\n");
-    }
- kruskal();
-}
-
-
-
-
+{	
+	int i,j;
+	printf("Enter the no.of vertex:\t");
+	scanf("%d",&n);
+	printf("Enter the adjacency matrix:\n");
+	for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+			scanf("%d",&cost[i][j]);
+	
+	printf("The adjacency matrix:\n");
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+			printf("%d\t",cost[i][j]);
+	printf("\n");
+	}
+	kruskal();
+}	
